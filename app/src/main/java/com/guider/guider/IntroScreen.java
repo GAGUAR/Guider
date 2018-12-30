833d292c1a71953c6077350c8c86e629d4a9eb6f
TheSijay <45512694+TheSijay@users.noreply.github.com>
2018-12-30 15:07:12 +0200

    Added splash screen activity

---
+++ app/src/main/java/com/guider/guider/IntroScreen.java
@@ -0,0 +1,33 @@
+package com.guider.guider;
+
+import android.content.Intent;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+
+public class IntroScreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro_screen);
+        Thread myThread = new Thread() {
+
+            @Override
+            public void run() {
+                try {
+                    sleep(3000);
+                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    startActivity(intent);
+                    finish();
+                } catch (InterruptedException e) {
+                    e.printStackTrace();
+                }
+            }
+        };
+        myThread.start();
+    }
+}
