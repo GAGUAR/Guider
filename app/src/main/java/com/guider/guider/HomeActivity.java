17860636725a7c7913c8bba4363680bf9616fd97
GAGUAR <mcarrow002@gmail.com>
2019-01-09 20:52:53 +0200

    Home screen added

---
+++ app/src/main/java/com/guider/guider/HomeActivity.java
@@ -0,0 +1,51 @@
+package com.guider.guider;
+
+import android.content.Intent;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class HomeActivity extends AppCompatActivity {
+    Button freewalk, routes, obj, lang;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_home);
+        freewalk=(Button)findViewById(R.id.freewalk);
+        routes=(Button)findViewById(R.id.routes);
+        obj=(Button)findViewById(R.id.obj);
+        lang=(Button)findViewById(R.id.lang);
+        buttons();
+    }
+
+    private void buttons() {
+        freewalk.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+        routes.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        obj.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+        lang.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
+    }
+}
--- app/src/main/java/com/guider/guider/IntroScreen.java
+++ app/src/main/java/com/guider/guider/IntroScreen.java
@@ -20,7 +20,7 @@ public class IntroScreen extends AppCompatActivity {
             public void run() {
                 try {
                     sleep(1000);
-                    Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                    Intent intent = new Intent(getApplicationContext(), HomeActivity.class);
                     startActivity(intent);
                     finish();
                 } catch (InterruptedException e) {
